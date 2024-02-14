bbd4c986cbfb0a9e1c771b424dca45935953851d
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-14 15:31:21 +0100

    Update cypress.config.js

--- tests-nodejs/cypress.config.js
+++ tests-nodejs/cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Inline Reporter',
+    embeddedScreenshots: true, 
+    inlineAssets: true,
+    reportDir: "cypress/report",
+  },
   e2e: {
     experimentalStudio: true,
     supportFile: 'web_cypress/support/e2e.{js,jsx,ts,tsx}',
