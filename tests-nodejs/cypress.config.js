684f7fbe5f5f0da1c889c795b158ee43b55fcc46
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-14 15:28:57 +0100

    Update cypress.config.js

--- tests-nodejs/cypress.config.js
+++ tests-nodejs/cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     experimentalStudio: true,
     supportFile: 'web_cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: 'web_cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
     fixturesFolder: 'web_cypress/fixtures',
     setupNodeEvents(on, config) { },
-  },
+    require('cypress-mochawesome-reporter/plugin')(on);
 });
