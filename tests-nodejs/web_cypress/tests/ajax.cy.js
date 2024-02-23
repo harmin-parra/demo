582dc4f170928e1d3a89d99b1462870633713498
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-23 16:14:01 +0100

    Add screenshots

--- tests-nodejs/web_cypress/tests/ajax.cy.js
+++ tests-nodejs/web_cypress/tests/ajax.cy.js
@@ -6,9 +6,12 @@ describe("Ajax page", () => {
   it("Ajax verification with intercept", () =>{
     cy.visit("https://harmin-demo.gitlab.io/reports/web/ajax.html")
     const page = new AjaxPage();
+    cy.screenshot();
     page.click();
+    cy.screenshot();
     cy.intercept("GET", "**/ajax.txt").as("ajaxResponse");
     cy.wait("@ajaxResponse", {timeout: 10000}).its("response.statusCode").should("equal", 200);
+    cy.screenshot();
     page.verify();
   });
 
@@ -16,11 +19,14 @@ describe("Ajax page", () => {
   it("Ajax verification with get+should", () =>{
     cy.visit("https://harmin-demo.gitlab.io/reports/web/ajax.html")
     const page = new AjaxPage();
+    cy.screenshot();
     page.click();
+    cy.screenshot();
     cy.get("#title", { timeout: 10000 }).should("exist");
     // cy.get("#title", { timeout: 10000 }).should(($input) => {
     //   expect($input).to.exist;
     // })
+    cy.screenshot();
     page.verify();
   });
 
