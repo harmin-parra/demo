4e2378957f9890a7fc195e449238822aae2048d0
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-02-23 16:08:40 +0100

    Add screenshots

--- tests-nodejs/web_cypress/tests/webform.cy.js
+++ tests-nodejs/web_cypress/tests/webform.cy.js
@@ -6,6 +6,7 @@ describe('Web Form', () => {
   it('fill in form', () => {
     cy.visit('https://www.selenium.dev/selenium/web/web-form.html')
     const page = new WebFormPage();
+    cy.screenshot();
     page.set_input("input");
     page.set_password("password");
     page.set_textarea("textarea");
@@ -15,7 +16,9 @@ describe('Web Form', () => {
     page.set_color("#00ff00");
     page.set_date("01/01/2024");
     page.set_range(1);
+    cy.screenshot();
     page.submit();
+    cy.screenshot();
   });
 
 });
