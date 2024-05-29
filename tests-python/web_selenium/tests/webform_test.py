0d99228bc3afe1b056403c55170559fd6712345c
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-05-29 23:52:54 +0200

    Update webform_test.py

--- tests-python/web_selenium/tests/webform_test.py
+++ tests-python/web_selenium/tests/webform_test.py
@@ -1,10 +1,21 @@
+import allure
 import os
 from web_selenium import pages
 
 
+@allure.parent_suite("Web interface (Playwright)")
+@allure.suite("Web Form")
+@allure.epic("Web interface (Playwright)")
+# @allure.story("Web Form")
+@allure.feature("Web Form")
 def test_fill_in_form(driver):
     """ Web form test """
     driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    allure.attach(
+        page.screenshot(full_page=True),
+        name="Empty form",
+        attachment_type=allure.attachment_type.PNG
+    )
     webform = pages.WebFormPage(driver)
     webform.set_input("login")
     webform.set_password("password")
@@ -15,4 +26,14 @@ def test_fill_in_form(driver):
     webform.set_color("#00ff00")
     webform.set_date("01/01/2024")
     webform.set_range(1)
+    allure.attach(
+        page.screenshot(full_page=True),
+        name="Complete form",
+        attachment_type=allure.attachment_type.PNG
+    )
     webform.submit()
+        allure.attach(
+        page.screenshot(full_page=True),
+        name="Submit form",
+        attachment_type=allure.attachment_type.PNG
+    )
