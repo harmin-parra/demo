a4afa592b5ba7008fe08db52563ac801651b7b6a
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-05-29 23:54:41 +0200

    Update ajax_test.py

--- tests-python/web_selenium/tests/ajax_test.py
+++ tests-python/web_selenium/tests/ajax_test.py
@@ -1,9 +1,30 @@
+import allure
 from web_selenium.conftest import driver
 from web_selenium import pages
 
 
+@allure.parent_suite("Web interface (Playwright)")
+@allure.suite("Ajax")
+@allure.epic("Web interface (Playwright)")
+#@allure.story("Ajax")
+@allure.feature("Ajax")
 def test_ajax_response(driver):
     driver.get("http://harmin-demo.gitlab.io/reports/web/ajax.html")
+    allure.attach(
+        page.screenshot(full_page=True),
+        name="Initial page",
+        attachment_type=allure.attachment_type.PNG
+    )
     ajax = pages.AjaxPage(driver)
     ajax.click()
+    allure.attach(
+        page.screenshot(full_page=True),
+        name="Trigger event",
+        attachment_type=allure.attachment_type.PNG
+    )
     ajax.verify()
+    allure.attach(
+        page.screenshot(full_page=True),
+        name="Verify event result",
+        attachment_type=allure.attachment_type.PNG
+    )
