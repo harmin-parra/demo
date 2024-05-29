1a01e320a9f5d1a56235dfd37f0efa1e1147aac3
Harmin <harmin.parra@gmail.com>
2024-05-29 10:07:54 +0200

    testing

--- tests-java/src/test/java/web_selenium/WebFormTest.java
+++ tests-java/src/test/java/web_selenium/WebFormTest.java
@@ -76,7 +76,6 @@ public class WebFormTest {
         page.set_number(2);
         page.set_city("Los Angeles");
         //page.set_file("src/test/resources/file.txt");
-        page.set_file("resources/file.txt");
         page.set_color("#00ff00");
         page.set_date("01/01/2024");
         page.set_range(1);
