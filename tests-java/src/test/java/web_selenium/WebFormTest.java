dd3357b5f418ad13ae9ed226fb6e3fbdf5d835fc
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-05-30 00:14:06 +0200

    Update WebFormTest.java

--- tests-java/src/test/java/web_selenium/WebFormTest.java
+++ tests-java/src/test/java/web_selenium/WebFormTest.java
@@ -32,7 +32,7 @@ public class WebFormTest {
 
     @BeforeEach
     public void setup() {
-        String browserName = StringUtils.isEmpty(System.getProperty("browser")) ? "chrome" : System.getProperty("browser");
+        String browserName = StringUtils.isEmpty(System.getProperty("browser")) ? "chromium" : System.getProperty("browser");
         String url = StringUtils.isEmpty(System.getProperty("hub")) ? "172.17.0.1" : System.getProperty("hub");
         URL hub = null;
         try {
