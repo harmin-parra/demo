537455d1621810965d1fa4dc1b97bcd0fd960170
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-05-30 17:45:02 +0200

    Update WebFormPage.java

--- tests-java/src/main/java/web_selenium/WebFormPage.java
+++ tests-java/src/main/java/web_selenium/WebFormPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class WebFormPage {
