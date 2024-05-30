05a3a0466d73252c44418d4bd5db4793f7101edd
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-05-30 10:10:00 +0200

    Update conftest.py

--- tests-python/web_selenium/conftest.py
+++ tests-python/web_selenium/conftest.py
@@ -5,8 +5,17 @@ from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
+def pytest_addoption(parser):
+    parser.addoption("--driver", action="store", default="chromium")
+
+
+@pytest.fixture(scope='session')
+def browser(request):
+    return request.config.getoption("--driver")
+
+
 @pytest.fixture(scope="function")
-def driver():
+def driver(browser):
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
