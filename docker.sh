6b250ddcd1edc79021ea1595e06c9ce34c8288de
harmin-parra <harmin.parra@gmail.com>
2024-07-04 23:17:35 +0200

    add scripts

---
+++ docker.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+DOCKER=""
+BROWSER="Chromium"
+HUB=""
+
+# Read command-line arguments
+while [ $# -gt 0 ]; do
+  case $1 in
+    --browser)
+      BROWSER="$2"
+      shift 2
+      ;;
+    --hub)
+      HUB="$2"
+      shift 2
+      ;;
+    --docker)
+      DOCKER="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Change variable values to lowercase
+BROWSER=${BROWSER,,}
+
+rm -rf reporting/*
+mkdir /tmp/test
+cp tests-python/file.xml /tmp/test/
+
+#
+# Python tests
+#
+if [ $DOCKER = "playwright/python" ] || [ $DOCKER = "selenium/python" ] || [ $DOCKER = "robotframework/playwright" ]; then
+  cd tests-python
+  pip install -r requirements.txt
+  playwright install-deps
+  playwright install
+  export PYTHONPATH=$(pwd)
+
+  if [ $DOCKER = "playwright/python" ]; then
+    behave cucumber/features/petstore.feature
+    pytest web_playwright/tests/ --browser $BROWSER
+  fi
+
+  if [ $DOCKER = "selenium/python" ]; then
+    pytest web_selenium/tests/ --driver $BROWSER --hub $HUB
+  fi
+
+  if [ $DOCKER = "robotframework/playwright" ]; then
+    robot --outputdir ../reporting/report-robot ./
+  fi
+
+  unset PYTHONPATH
+  cd ..
+fi
+
+#
+# Node.js tests
+#
+if [ $DOCKER = "playwright/node.js" ] || [ $DOCKER = "cypress" ]; then
+  cd tests-nodejs
+  npm install
+  npx playwright install-deps
+  npx playwright install
+
+  if [ $DOCKER = "playwright/node.js" ]; then
+    npx cucumber-js cucumber/features/petstore.feature
+    npx playwright test --project $BROWSER
+
+  else
+    npx cypress run --browser $BROWSER --headless
+  fi
+
+  cd ..
+fi
+
+
+#
+# Java tests
+#
+if [ $DOCKER = "playwright/java" ] || [ $DOCKER = "selenium/java" ] || [ $DOCKER = "karate" ]; then
+  cd tests-java
+  mvn dependency:resolve
+
+  if [ $DOCKER = "playwright/java" ]; then
+    mvn -Dtest="web_playwright/**, rest_api_rest_assured/**" -Dbrowser=$BROWSER test
+
+  elif [ $DOCKER = "selenium/java" ]; then
+    mvn -Dtest="web_selenium/**" -Dbrowser=$BROWSER test -Dhub=$HUB test
+
+  elif [ $DOCKER = "karate" ]; then
+    mvn -Dtest="karate/TestRunner#allTests" -Dbrowser=$BROWSER test
+    for filename in ../reporting/allure-results/java/*result.json; do
+      RES=$(egrep '"testCaseName":"\[[0-9]+:[0-9]+\]' $filename)
+      if [ -n "$RES" ]; then
+        rm -f $filename
+      fi
+    done
+    mv tests-java/target/karate-reports reporting/report-karate
+  fi
+
+  cd ..
+fi
---
+++ runner.sh
@@ -0,0 +1,92 @@
+BROWSER="Chromium"
+HUB="localhost"
+
+# Read command-line arguments
+while [ $# -gt 0 ]; do
+  case $1 in
+    --browser)
+      BROWSER="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Change variable values to lowercase
+BROWSER=${BROWSER,,}
+
+
+rm -rf reporting/*
+mkdir /tmp/test
+cp tests-python/file.xml /tmp/test/
+
+
+#
+# Python tests
+#
+python3 --version
+cd tests-python
+export PYTHONPATH=$(pwd)
+
+pip install --break-system-packages -r requirements.txt
+# playwright install-deps
+playwright install
+rfbrowser init
+
+behave cucumber/features/petstore.feature
+# pytest web_playwright/tests/webform_test.py
+pytest web_playwright/tests/ --browser $BROWSER
+pytest web_selenium/tests/ --driver $BROWSER  # --hub $HUB
+robot --outputdir ../reporting/report-robot ./
+  # --listener allure_robotframework:../reporting/allure-results/python \
+  # --outputdir ../reporting/report-robot ./
+
+unset PYTHONPATH
+cd ..
+
+
+#
+# Node.js tests
+#
+nodejs --version
+npm --version
+cd tests-nodejs
+
+npm install
+# npx playwright install-deps
+npx playwright install
+
+npx cucumber-js cucumber/features/petstore.feature
+# npx playwright test webform.spec.ts
+npx playwright test --project $BROWSER
+# npx cypress run --spec web_cypress/tests/webform.cy.js
+npx cypress run --browser $BROWSER --headless
+
+cd ..
+
+
+#
+# Java tests
+#
+java --version
+cd tests-java
+
+mvn dependency:resolve
+
+# mvn -Dtest="web_playwright/WebFormTest" test
+mvn -Dtest="web_playwright/**, rest_api_rest_assured/**" -Dbrowser=$BROWSER test
+mvn -Dtest="web_selenium/**" -Dbrowser=$BROWSER test  # -Dhub=$HUB test
+# mvn -Dtest="web_karate/**, rest_api_karate/**" -Dbrowser="chrome" test
+mvn -Dtest="karate/TestRunner#allTests" -Dbrowser=$BROWSER test
+for filename in ../reporting/allure-results/java/*result.json; do
+  RES=$(egrep '"testCaseName":"\[[0-9]+:[0-9]+\]' $filename)
+  if [ -n "$RES" ]; then
+    rm -f $filename
+  fi
+done
+
+cd ..
+mv tests-java/target/karate-reports reporting/report-karate
