7edca0525f0c32622fa592eb4438cb81993cfc32
Harmin Parra Rueda <harmin.rueda@gmail.com>
2024-07-05 06:00:04 +0200

    Update allure.sh

--- allure.sh
+++ allure.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-LANG="All"
 EXECUTOR_NAME="Github Actions"
 EXECUTOR_TYPE="github"
 
 # Read command-line arguments
 while [ $# -gt 0 ]; do
   case $1 in
-    --lang)
-      LANG="$2"
-      shift 2
-      ;;
     --browser)
       BROWSER="$2"
       shift 2
@@ -22,117 +17,92 @@ while [ $# -gt 0 ]; do
   esac
 done
 
-LANG=${LANG,,}
+# Browser with initial uppercase
 BROWSER=${BROWSER,,}
 BROWSER=${BROWSER^}
 
 #
 # Java tests
 #
-if [ $LANG == "java" ] || [ $LANG == "all" ]; then
-  cat << EOF > reporting/allure-results/java1/environment.properties
+cat << EOF > reporting/allure-results/java/environment.properties
 Browser = $BROWSER
 OpenJDK = 17
+Selenium = 4.21.0
 Playwright = 1.44.0
-Cucumber-JVM = 7.15.0
 Rest-assured = 5.4.0
+Karate = 1.4.1
 EOF
+#Cucumber-JVM = 7.15.0
 
-  if [ -f reporting/allure-results/java1/job.url ]; then
-    cat << EOF > reporting/allure-results/java1/executor.json
+if [ -f reporting/allure-results/java/job.url ]; then
+  cat << EOF > reporting/allure-results/java/executor.json
 {
   "name": "${EXECUTOR_NAME}",
   "type": "${EXECUTOR_TYPE}",
   "buildName": "Build log",
-  "buildUrl": "$(cat reporting/allure-results/java1/job.url)",
+  "buildUrl": "$(cat reporting/allure-results/java/job.url)",
   "reportName": "Demo Java report"
 }
 EOF
-  fi
-
-  if [ -f reporting/allure-results/java2/job.url ]; then
-    cat << EOF > reporting/allure-results/java2/executor.json
-{
-  "name": "${EXECUTOR_NAME}",
-  "type": "${EXECUTOR_TYPE}",
-  "buildName": "Build log",
-  "buildUrl": "$(cat reporting/allure-results/java2/job.url)",
-  "reportName": "Demo Java report"
-}
-EOF
-  fi
-
-  allure generate \
-    --clean \
-    --output reporting/allure-reports/report-java \
-    --single-file reporting/allure-results/java1 reporting/allure-results/java2
 fi
+
+allure generate \
+  --clean \
+  --output reporting/allure-reports/report-java \
+  --single-file reporting/allure-results/java
 
 #
 # Node.js tests
 #
-if [ $LANG == "node.js" ] || [ $LANG == "all" ]; then
-  cat << EOF > reporting/allure-results/nodejs-results/environment.properties
+cat << EOF > reporting/allure-results/nodejs/environment.properties
 Browser = $BROWSER
 Node.js = 20.13.1
 Playwright = 1.44.1
 Cucumber.js = 10.3.1
 EOF
 
-  cat << EOF > reporting/allure-results/nodejs-results/executor.json
+if [ -f reporting/allure-results/nodejs/job.url ]; then
+  cat << EOF > reporting/allure-results/nodejs/executor.json
 {
   "name": "${EXECUTOR_NAME}",
   "type": "${EXECUTOR_TYPE}",
   "buildName": "Build log",
-  "buildUrl": "${JOB_NODEJS}",
+  "buildUrl": "$(cat reporting/allure-results/nodejs/job.url)",
   "reportName": "Demo Node.js report"
 }
 EOF
-
-  allure generate \
-    --clean \
-    --output reporting/allure-reports/report-nodejs \
-    --single-file reporting/allure-results/nodejs-results
 fi
+
+allure generate \
+  --clean \
+  --output reporting/allure-reports/report-nodejs \
+  --single-file reporting/allure-results/nodejs
 
 #
 # Python tests
 #
-if [ $LANG == "python" ] || [ $LANG == "all" ]; then
-  cat << EOF > reporting/allure-results/python1/environment.properties
+cat << EOF > reporting/allure-results/python/environment.properties
 Browser = $BROWSER
 Python = 3.10.12
-Playwright = 1.44.0
 Behave = 1.2.6
+Selenium = 4.21.0
+Playwright = 1.44.0
+RobotFramework = 7.0
 EOF
 
-  if [ -f reporting/allure-results/python1/job.url ]; then
-    cat << EOF > reporting/allure-results/python1/executor.json
+if [ -f reporting/allure-results/python/job.url ]; then
+  cat << EOF > reporting/allure-results/python/executor.json
 {
   "name": "${EXECUTOR_NAME}",
   "type": "${EXECUTOR_TYPE}",
   "buildName": "Build log",
-  "buildUrl": "$(cat reporting/allure-results/python1/job.url)",
+  "buildUrl": "$(cat reporting/allure-results/python/job.url)",
   "reportName": "Demo Python report"
 }
 EOF
-  fi
-
-  if [ -f reporting/allure-results/python2/job.url ]; then
-    cat << EOF > reporting/allure-results/python2/executor.json
-{
-  "name": "${EXECUTOR_NAME}",
-  "type": "${EXECUTOR_TYPE}",
-  "buildName": "Build log",
-  "buildUrl": "$(cat reporting/allure-results/python2/job.url)",
-  "reportName": "Demo Python report"
-}
-EOF
-  fi
-
-  allure generate \
-    --clean \
-    --output reporting/allure-reports/report-python \
-    --single-file reporting/allure-results/python1 reporting/allure-results/python2
-
 fi
+
+allure generate \
+  --clean \
+  --output reporting/allure-reports/report-python \
+  --single-file reporting/allure-results/python
